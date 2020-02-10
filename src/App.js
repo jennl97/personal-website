3510a511ed6df3471f1855bcc9a326f396c59695
jennl97 <jennhott97@gmail.com>
2020-02-10 13:26:14 -0600

    Initial commit of new project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi there!</h1>
+        <h2>My name is Jenn!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
